562ca87b47ceb628e4c9507abde431bc1ef1eb78
hvsr1997 <rathoreharshvardhansingh1997@gmail.com>
2024-01-10 08:11:34 +0530

    pawn attack move

--- src/com/chess/engine/piece/Pawn.java
+++ src/com/chess/engine/piece/Pawn.java
@@ -5,6 +5,7 @@ import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
 import com.chess.engine.board.Move.MajorMove;
+import com.google.common.collect.ImmutableList;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -12,7 +13,7 @@ import java.util.List;
 
 public class Pawn extends Piece{
 
-    private final static int[] CANDIDATE_MOVE_COORDINATE = {8,16};
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {8,16,7,9};
 
     Pawn(final int piecePosition,final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
@@ -41,8 +42,33 @@ public class Pawn extends Piece{
                 !board.getTile(candidateDestinationCoordinate).isTileOccupied())  {
                     legalMoves.add(new MajorMove(board,this,candidateDestinationCoordinate));
                 }
+            } else if(currentCandidateOffset == 7 &&
+                    !((BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite()) ||
+                        (BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack()))) {
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                    final Piece pieceAtCoordinate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if(this.pieceAlliance != pieceAtCoordinate.getPieceAlliance()){
+                        //todo attack for pawn promotion
+                        legalMoves.add(new MajorMove(board,this,candidateDestinationCoordinate));
+
+                    }
+                }
+
+            } else if(currentCandidateOffset == 9 &&
+                    !((BoardUtils.FIRST_COLUMN[this.piecePosition] && this.pieceAlliance.isWhite()) ||
+                            (BoardUtils.EIGHTH_COLUMN[this.piecePosition] && this.pieceAlliance.isBlack()))) {
+
+                if(board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                    final Piece pieceAtCoordinate = board.getTile(candidateDestinationCoordinate).getPiece();
+                    if(this.pieceAlliance != pieceAtCoordinate.getPieceAlliance()){
+                        //todo attack for pawn promotion
+                        legalMoves.add(new MajorMove(board,this,candidateDestinationCoordinate));
+
+                    }
+                }
+
             }
         }
-        return legalMoves;
+        return ImmutableList.copyOf(legalMoves);
     }
 }
