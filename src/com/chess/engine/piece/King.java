d2cc57789db2a91b68d1f6ea7f45cdc1c4bedc03
hvsr1997 <rathoreharshvardhansingh1997@gmail.com>
2024-01-11 09:20:51 +0530

    tracking pieces on board and some refactoring

--- src/com/chess/engine/piece/King.java
+++ src/com/chess/engine/piece/King.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class King extends Piece{
 
     private final static int[] CANDIDATE_MOVE_COORDINATE = {9,8,7,1,-1,-7,-8,-9};
-    King(int piecePosition, Alliance pieceAlliance) {
+    public King(Alliance pieceAlliance, int piecePosition) {
         super(piecePosition, pieceAlliance);
     }
 
