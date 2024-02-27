5705f40b177fa89b93c993d2e4ead9fadb8974b2
hvsr1997 <rathoreharshvardhansingh1997@gmail.com>
2024-02-27 07:24:59 +0530

    added hashcode to class

--- src/com/chess/engine/piece/Piece.java
+++ src/com/chess/engine/piece/Piece.java
@@ -12,6 +12,7 @@ public abstract class Piece {
 
     protected final boolean isFirstMove;
     protected final PieceType pieceType;
+    private final int cachedHashCode;
 
     Piece(final PieceType pieceType, final int piecePosition, final Alliance pieceAlliance) {
         this.pieceType = pieceType;
@@ -19,6 +20,35 @@ public abstract class Piece {
         this.piecePosition = piecePosition;
         //todo
         this.isFirstMove = false;
+        this.cachedHashCode = computeHashCode();
+    }
+
+    private int computeHashCode() {
+        int result = pieceType.hashCode();
+        result = 31*result+ pieceAlliance.hashCode();
+        result = 31*result+ piecePosition;
+        result = 31*result+ (isFirstMove?1:0);
+        return result;
+
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        if(this == other) {
+            return true;
+        }
+        if(!(other instanceof Piece)) {
+             return false;
+        }
+        final Piece otherPiece = (Piece) other;
+        return piecePosition == otherPiece.getPiecePosition() && pieceType == otherPiece.getPieceType() &&
+                pieceAlliance == otherPiece.getPieceAlliance() && isFirstMove == otherPiece.isFirstMove();
+
+    }
+
+    @Override
+    public int hashCode() {
+        return this.cachedHashCode;
     }
 
     public int getPiecePosition() {
